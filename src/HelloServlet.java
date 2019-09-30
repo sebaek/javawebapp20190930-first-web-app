fb48bb00f08640d1845b80dec720a73783e29757
choongang602 <choongang602@exmap.com>
2019-09-30 14:51:10 +0900

    first commit

---
+++ src/HelloServlet.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<meta charset='utf-8'>");
+		out.print("<title>first servlet</title>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<h1>");
+		out.print("안녕하세요 첫 번째 서블릿입니다.");
+		out.print("</h1>"); 
+		out.print("<p>");
+		out.print(new Date());
+		out.print("</p>");
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
